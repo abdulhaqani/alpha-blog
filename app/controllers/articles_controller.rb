3c2ed0f12cdc469dec18b11e967632670318fa1d
abdulhaqani <ahaqa011@uottawa.ca>
2020-06-13 17:21:14 -0400

    flash and create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   def new
-    
+    @article = Article.new
   end
   def create
-    render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end

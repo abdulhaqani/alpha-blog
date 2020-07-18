e5d2fb0a5f32bfde71e9c2aabad83e856608d6b0
abdulhaqani <ahaqa011@uottawa.ca>
2020-07-18 16:33:37 -0400

    authentication restrictions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,4 @@ module ApplicationHelper
       alt: user.username, class: 'rounded shadow mx-auto d-block'
     )
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id] # or=
-  end
-
-  def logged_in?
-    !!current_user
-  end
 end

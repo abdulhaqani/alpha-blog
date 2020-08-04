4903fdf4189386fdb62f47e78ec1f0efac8c30cc
abdulhaqani <ahaqa011@uottawa.ca>
2020-08-04 16:58:37 -0400

    final commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,22 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = 'The category was successfully updated'
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   private
